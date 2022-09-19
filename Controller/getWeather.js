34ec708adcb386b049a3745bc713f2ae32bd03d7
yamana113 <mathisfabre2@gmail.com>
2022-09-19 12:07:06 +0200

    api marche

--- Controller/getWeather.js
+++ Controller/getWeather.js
@@ -1,4 +1,10 @@
+$(document).ready(() => {
+    console.log("ready");
+    $("#weather_btn").click(() => { 
+        getLocation($("#cityInput").val()).then(data => getWeather(data.lat, data.lng));
 
+    });
+})
 
 async function getLocation(cityName) {
     let response = await fetch("https://maps.googleapis.com/maps/api/geocode/json?address="+cityName+"&key=AIzaSyDdVJ6zE0-ZSqncFEb2H9oJe-hn05-kMqg");
@@ -6,11 +12,8 @@ async function getLocation(cityName) {
     return data["results"][0]["geometry"]["location"];
 }
 
-async function getWeather(cityName) {
-    location = getLocation()
-    let response = await fetch("https://api.open-meteo.com/v1/forecast?latitude=43.23&longitude=5.44&hourly=temperature_2m");
-    let data = await response.text();
+async function getWeather(latitude, longitude) {
+    let response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m`);
+    let data = await response.json();
     console.log(data);
 }
-
-getLocation("Marseille");
