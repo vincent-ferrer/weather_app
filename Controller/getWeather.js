1e444c8bd4604491292d7eba742c7715caa4d523
Vincent Ferrer <45425884+vincent-ferrer@users.noreply.github.com>
2022-09-19 12:29:58 +0200

    console temperature actuelle

--- Controller/getWeather.js
+++ Controller/getWeather.js
@@ -7,13 +7,14 @@ $(document).ready(() => {
 })
 
 async function getLocation(cityName) {
-    let response = await fetch("https://maps.googleapis.com/maps/api/geocode/json?address="+cityName+"&key=AIzaSyDdVJ6zE0-ZSqncFEb2H9oJe-hn05-kMqg");
+    let response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${cityName}&key=AIzaSyDdVJ6zE0-ZSqncFEb2H9oJe-hn05-kMqg`);
     let data = await response.json();
     return data["results"][0]["geometry"]["location"];
 }
 
 async function getWeather(latitude, longitude) {
-    let response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m`);
+    // let response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m`);
+    let response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true&timezone=Europe%2FBerlin`)
     let data = await response.json();
-    console.log(data);
+    console.log(data['current_weather']['temperature']);
 }
