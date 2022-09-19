e4e0e641a624aead77df8c36ec1d7e6ef539cfe5
Vincent Ferrer <45425884+vincent-ferrer@users.noreply.github.com>
2022-09-19 10:52:27 +0200

    Console OK

--- Controller/getWeather.js
+++ Controller/getWeather.js
@@ -4,7 +4,8 @@ function getLocation(cityName) {
     return (0,0);
 }
 
-async function getWeather(latitude, longitude) {
+async function getWeather(cityName) {
+    location = getLocation()
     let response = await fetch("https://api.open-meteo.com/v1/forecast?latitude=43.23&longitude=5.44&hourly=temperature_2m");
     let data = await response.text();
     console.log(data);
