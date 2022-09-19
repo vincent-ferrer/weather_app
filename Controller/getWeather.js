562aa971807a1bdfdac25774fe3b473bac6daca5
yamana113 <mathis.fabre@etu.univ-amu.fr>
2022-09-19 14:16:39 +0200

    debut table

--- Controller/getWeather.js
+++ Controller/getWeather.js
@@ -2,7 +2,7 @@ $(document).ready(() => {
     console.log("ready");
     $("#weather_btn").click(() => {
         getLocation($("#cityInput").val()).then(data => getWeather(data.lat, data.lng, 1));
-
+        createWeatherTable()
     });
 })
 
@@ -23,4 +23,36 @@ async function getCurrentWeatherOpenMeteo(latitude, longitude) {
     let response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true&timezone=Europe%2FBerlin`)
     let data = await response.json();
     return data['current_weather'];
+}
+
+let tableHeaders = ["Temperature", "vent", "API"]
+
+const tableDiv = $("#weatherTable")
+
+function createWeatherTable() {
+    while(tableDiv.firstChild) tableDiv.removeChild(tableDiv.firstChild);
+
+    let weatherTable = document.createElement('table')
+    weatherTable.className = "weatherTable"
+
+    let weatherTableHeader = document.createElement('thead')
+    weatherTableHeader.className = "weatherTableHeader"
+
+    let weatherTableHeaderRow = document.createElement('tr')
+    weatherTableHeaderRow.className = "weatherTableHeaderRow"
+
+    tableHeaders.forEach(header => {
+        let tableHeader = document.createElement('th')
+        tableHeader.innerHTML = header
+        weatherTableHeaderRow.append(tableHeader)
+    })
+
+    weatherTableHeader.append(weatherTableHeaderRow)
+    weatherTable.append(weatherTableHeader)
+
+    let weatherTableBody = document.createElement('tbody')
+    weatherTableBody.className = "weatherTableBody"
+    weatherTable.append(weatherTableBody)
+
+    tableDiv.append(weatherTable)
 }
