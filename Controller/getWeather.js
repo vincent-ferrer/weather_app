fb508bf0776140b8b2127b6c4a8677ff784f60f9
yamana113 <mathis.fabre@etu.univ-amu.fr>
2022-09-19 15:03:54 +0200

    ajout donnée table

--- Controller/getWeather.js
+++ Controller/getWeather.js
@@ -1,8 +1,9 @@
 $(document).ready(() => {
     console.log("ready");
     $("#weather_btn").click(() => {
-        getLocation($("#cityInput").val()).then(data => getWeather(data.lat, data.lng, 1));
         createWeatherTable()
+
+        getWeather($("#cityInput").val(), 2).then(data => fillWeatherTable(data))
     });
 })
 
@@ -12,42 +13,51 @@ async function getLocation(cityName) {
     return data["results"][0]["geometry"]["location"];
 }
 
-async function getWeather(latitude, longitude, nbServeur) {
-    meteo = getCurrentWeatherOpenMeteo(latitude,longitude).then(
-        data => console.log(data)
-    )
+async function getWeather(cityName, nbServeur) {
+    let data = []
+    let coordinates = await getLocation(cityName)
+    console.log(coordinates)
+
+    data.push(await getCurrentWeatherOpenMeteo(coordinates.lat,coordinates.lng))
+    data.push(await getCurrentWeatherVisualCrossing(cityName))
+    data.push(await getCurrentWeatherBit(coordinates.lat,coordinates.lng))
+
+    console.log(data)
+    return data
 }
 
 async function getCurrentWeatherOpenMeteo(latitude, longitude) {
     // let response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m`);
     let response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true&timezone=Europe%2FBerlin`)
     let data = await response.json();
-    let result = '{ "temperature" : '+data["current_weather"]["temperature"]+',' +
-        ' "vent" :'+data["current_weather"]["windspeed"]+',' +
-        ' "serveur" :"open-meteo"}';
-    return JSON.parse(result);
+
+    return {
+        temperature: data["current_weather"]["temperature"],
+        vent: data["current_weather"]["windspeed"],
+        serveur: "open-meteo"
+    }
 }
 
 async function getCurrentWeatherVisualCrossing(cityName) {
     let response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${cityName}?unitGroup=metric&key=LLBEM9ZL9N349DP3EGU5UB522&contentType=json`)
     let data = await response.json();
 
-    let result = '{ "temperature" : '+data["currentConditions"]["temp"]+',' +
-        ' "vent" :'+data["currentConditions"]["windspeed"]+',' +
-        ' "serveur" :"weather visualcrossing"}';
-
-    return JSON.parse(result);
+    return {
+        temperature: data["currentConditions"]["temp"],
+        vent: data["currentConditions"]["windspeed"],
+        serveur: "weather visualcrossing"
+    }
 }
 
-async function getCurrentWeatherBit(cityName) {
-    let loc = await getLocation(cityName).then();
-    let response = await fetch(`https://api.weatherbit.io/v2.0/current?lat=${loc["lat"]}&lon=${loc["lng"]}&key=92e7dcd0cafc46c7b3c2b3e19c93c3e4`)
+async function getCurrentWeatherBit(latitude, longitude) {
+    let response = await fetch(`https://api.weatherbit.io/v2.0/current?lat=${latitude}&lon=${longitude}&key=92e7dcd0cafc46c7b3c2b3e19c93c3e4`)
     let data = await response.json();
-    let result = '{ "temperature" : ' + data["data"][0]["temp"] + ',' +
-        ' "vent" :' + data["data"][0]["wind_spd"] + ',' +
-        ' "serveur" :"weather weather bit"}';
 
-    return JSON.parse(result);
+    return {
+        temperature: data["data"][0]["temp"],
+        vent: data["data"][0]["wind_spd"],
+        serveur: "weather bit"
+    }
 }
 
 let tableHeaders = ["Temperature", "vent", "API"]
@@ -77,7 +87,29 @@ function createWeatherTable() {
 
     let weatherTableBody = document.createElement('tbody')
     weatherTableBody.className = "weatherTableBody"
+    weatherTableBody.id = "weatherTableBody"
     weatherTable.append(weatherTableBody)
 
     tableDiv.append(weatherTable)
+}
+
+function fillWeatherTable(data) {
+    const weatherTableBody = $("#weatherTableBody")
+
+    data.forEach(data => {
+        let weatherRow = document.createElement('tr')
+        weatherRow.className = "weatherRow"
+
+        let temp = document.createElement('td')
+        temp.innerText = data["temperature"]
+
+        let wind = document.createElement('td')
+        wind.innerText = data["vent"]
+
+        let api = document.createElement('td')
+        api.innerText = data["serveur"]
+
+        weatherRow.append(temp, wind, api)
+        weatherTableBody.append(weatherRow)
+    })
 }
