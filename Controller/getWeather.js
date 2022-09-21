764c354e9333e5bfffec704bc0cafa37290923b9
Vincent Ferrer <45425884+vincent-ferrer@users.noreply.github.com>
2022-09-21 08:57:58 +0200

    Ajout du temps de reponse des serveurs

--- Controller/getWeather.js
+++ Controller/getWeather.js
@@ -4,7 +4,7 @@ $(document).ready(() => {
     $("#weather_btn").click(() => {
         $("#weatherTableBody").html("")
 
-        getWeather($("#cityInput").val(), 2).then(data => fillWeatherTable(data))
+        getWeather($("#cityInput").val()).then(data => fillWeatherTable(data))
     });
 })
 
@@ -14,7 +14,7 @@ async function getLocation(cityName) {
     return data["results"][0]["geometry"]["location"];
 }
 
-async function getWeather(cityName, nbServeur) {
+async function getWeather(cityName) {
     let data = []
     let coordinates = await getLocation(cityName)
     console.log(coordinates)
@@ -29,40 +29,47 @@ async function getWeather(cityName, nbServeur) {
 }
 
 async function getCurrentWeatherOpenMeteo(latitude, longitude) {
-    // let response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m`);
+    let tempsRep = Date.now();
     let response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true&timezone=Europe%2FBerlin`)
     let data = await response.json();
+    tempsRep = Math.floor(Date.now() - tempsRep);
 
     return {
         temperature: data["current_weather"]["temperature"],
         vent: data["current_weather"]["windspeed"],
-        serveur: "open-meteo"
+        serveur: "open-meteo",
+        tempsReponseServeur: tempsRep
     }
 }
 
 async function getCurrentWeatherVisualCrossing(cityName) {
+    let tempsRep = Date.now();
     let response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${cityName}?unitGroup=metric&key=LLBEM9ZL9N349DP3EGU5UB522&contentType=json`)
     let data = await response.json();
-
+    tempsRep = Math.floor(Date.now() - tempsRep);
+    console.log(tempsRep)
     return {
         temperature: data["currentConditions"]["temp"],
         vent: data["currentConditions"]["windspeed"],
-        serveur: "weather visualcrossing"
+        serveur: "weather visualcrossing",
+        tempsReponseServeur: tempsRep
     }
 }
 
 async function getCurrentWeatherBit(latitude, longitude) {
+    let tempsRep = Date.now();
     let response = await fetch(`https://api.weatherbit.io/v2.0/current?lat=${latitude}&lon=${longitude}&key=92e7dcd0cafc46c7b3c2b3e19c93c3e4`)
     let data = await response.json();
-
+    tempsRep = Math.floor(Date.now() - tempsRep);
     return {
         temperature: data["data"][0]["temp"],
         vent: data["data"][0]["wind_spd"]*3.6, // m/s -> km/h
-        serveur: "weather bit"
+        serveur: "weather bit",
+        tempsReponseServeur: tempsRep
     }
 }
 
-let tableHeaders = ["Temperature", "vent", "API"]
+let tableHeaders = ["Temperature", "vent", "API","Temps de reponse (ms)"]
 
 const tableDiv = $("#weatherTable")
 
@@ -111,7 +118,10 @@ function fillWeatherTable(data) {
         let api = document.createElement('td')
         api.innerText = data["serveur"]
 
-        weatherRow.append(temp, wind, api)
+        let tempsReponse = document.createElement('td')
+        tempsReponse.innerText = data["tempsReponseServeur"]
+
+        weatherRow.append(temp, wind, api, tempsReponse)
         weatherTableBody.append(weatherRow)
     })
 }
@@ -119,8 +129,6 @@ function fillWeatherTable(data) {
 function calculConsensus(datas) {
     temp = 0;
     vent = 0;
-
-    console.log(datas)
 
     datas.forEach(datas=>{
         temp += datas["temperature"]
@@ -133,6 +141,7 @@ function calculConsensus(datas) {
     return {
         temperature: temp,
         vent: vent,
-        serveur: "consensus"
+        serveur: "consensus",
+        tempsReponseServeur: 0
     }
 }
